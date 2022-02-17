a14323598c3a55448714f8fa8253318cb763f568
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2022-02-17 19:31:38 +0100

    Add linters

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This repository uses [Zola](https://www.getzola.org/documentation/getting-started/installation/) to generate static websites. Requires zola version 0.15.3.
 
 Place new lessons in the `content` directory.
+
+Before making a PR, please run the following commands:
+
+```sh
+zola check
+cargo clippy --all-targets --all-features -- -D warnings
+cargo fmt --all --check
+cargo test --all-targets --all-features --no-fail-fast
+npx stylelint "**/*.{scss, css}" --fix
+npx eslint "**/*.{js, ts}" --fix
+npx prettier --write .
+```
--- static/book.js
+++ static/book.js
@@ -1,3 +1,5 @@
+let elasticlunr;
+
 function initToggleMenu() {
   const $menu = document.querySelector(".menu");
   const $menuIcon = document.querySelector(".menu-icon");
@@ -39,23 +41,23 @@ function makeTeaser(body, terms) {
   const FIRST_WORD_WEIGHT = 8;
   const TEASER_MAX_WORDS = 30;
 
-  const stemmedTerms = terms.map(w => elasticlunr.stemmer(w.toLowerCase()));
+  const stemmedTerms = terms.map((w) => elasticlunr.stemmer(w.toLowerCase()));
   let termFound = false;
   let index = 0;
-  let weighted = []; // contains elements of ["word", weight, index_in_document]
+  const weighted = []; // contains elements of ["word", weight, index_in_document]
 
   // split in sentences, then words
-  let sentences = body.toLowerCase().split(". ");
+  const sentences = body.toLowerCase().split(". ");
 
-  for (let i in sentences) {
-    let words = sentences[i].split(" ");
+  for (const i in sentences) {
+    const words = sentences[i].split(" ");
     let value = FIRST_WORD_WEIGHT;
 
-    for (let j in words) {
-      let word = words[j];
+    for (const j in words) {
+      const word = words[j];
 
       if (word.length > 0) {
-        for (let k in stemmedTerms) {
+        for (const k in stemmedTerms) {
           if (elasticlunr.stemmer(word).startsWith(stemmedTerms[k])) {
             value = TERM_WEIGHT;
             termFound = true;
@@ -66,18 +68,18 @@ function makeTeaser(body, terms) {
       }
 
       index += word.length;
-      index += 1;  // ' ' or '.' if last word in sentence
+      index += 1; // ' ' or '.' if last word in sentence
     }
 
-    index += 1;  // because we split at a two-char boundary '. '
+    index += 1; // because we split at a two-char boundary '. '
   }
 
   if (weighted.length === 0) {
     return body;
   }
 
-  let windowWeights = [];
-  let windowSize = Math.min(weighted.length, TEASER_MAX_WORDS);
+  const windowWeights = [];
+  const windowSize = Math.min(weighted.length, TEASER_MAX_WORDS);
   // We add a window with all the weights first
   let curSum = 0;
   for (let i = 0; i < windowSize; i++) {
@@ -104,10 +106,10 @@ function makeTeaser(body, terms) {
     }
   }
 
-  let teaser = [];
+  const teaser = [];
   let startIndex = weighted[maxSumIndex][2];
   for (let i = maxSumIndex; i < maxSumIndex + windowSize; i++) {
-    let word = weighted[i];
+    const word = weighted[i];
     if (startIndex < word[2]) {
       // missing text from index to start of `word`
       teaser.push(body.substring(startIndex, word[2]));
@@ -130,18 +132,20 @@ function makeTeaser(body, terms) {
 }
 
 function formatSearchResultItem(item, terms) {
-  let li = document.createElement("li");
+  const li = document.createElement("li");
   li.classList.add("search-results__item");
   li.innerHTML = `<a href="${item.ref}">${item.doc.title}</a>`;
   li.innerHTML += `<div class="search-results__teaser">${makeTeaser(
-      item.doc.body, terms)}</div>`;
+    item.doc.body,
+    terms
+  )}</div>`;
   return li;
 }
 
 // Go from the book view to the search view
 function toggleSearchMode() {
-  let $bookContent = document.querySelector(".book-content");
-  let $searchContainer = document.querySelector(".search-container");
+  const $bookContent = document.querySelector(".book-content");
+  const $searchContainer = document.querySelector(".search-container");
   if ($searchContainer.classList.contains("search-container--is-visible")) {
     $searchContainer.classList.remove("search-container--is-visible");
     document.body.classList.remove("search-mode");
@@ -155,60 +159,69 @@ function toggleSearchMode() {
 }
 
 function initSearch() {
-  let $searchInput = document.getElementById("search");
+  const $searchInput = document.getElementById("search");
   if (!$searchInput) {
     return;
   }
-  let $searchIcon = document.querySelector(".search-icon");
+  const $searchIcon = document.querySelector(".search-icon");
   $searchIcon.addEventListener("click", toggleSearchMode);
 
-  let $searchResults = document.querySelector(".search-results");
-  let $searchResultsHeader = document.querySelector(".search-results__header");
-  let $searchResultsItems = document.querySelector(".search-results__items");
+  const $searchResults = document.querySelector(".search-results");
+  const $searchResultsHeader = document.querySelector(
+    ".search-results__header"
+  );
+  const $searchResultsItems = document.querySelector(".search-results__items");
   const MAX_ITEMS = 10;
 
-  let options = {
+  const options = {
     bool: "AND",
     fields: {
-      title: {boost: 2},
-      body: {boost: 1},
-    }
+      title: { boost: 2 },
+      body: { boost: 1 },
+    },
   };
   let currentTerm = "";
-  let index = elasticlunr.Index.load(window.searchIndex);
+  const index = elasticlunr.Index.load(window.searchIndex);
 
-  $searchInput.addEventListener("keyup", debounce(() => {
-    let term = $searchInput.value.trim();
-    if (term === currentTerm || !index) {
-      return;
-    }
-    $searchResults.style.display = term === "" ? "none" : "block";
-    $searchResultsItems.innerHTML = "";
-    if (term === "") {
-      return;
-    }
-
-    let results = index.search(term, options).filter((r) => r.doc.body !== "");
-    if (results.length === 0) {
-      $searchResultsHeader.innerText = `No search results for '${term}'.`;
-      return;
-    }
-
-    currentTerm = term;
-    $searchResultsHeader.innerText = `${results.length} search results for '${term}':`;
-    for (let i = 0; i < Math.min(results.length, MAX_ITEMS); i++) {
-      if (!results[i].doc.body) {
-        continue;
+  $searchInput.addEventListener(
+    "keyup",
+    debounce(() => {
+      const term = $searchInput.value.trim();
+      if (term === currentTerm || !index) {
+        return;
+      }
+      $searchResults.style.display = term === "" ? "none" : "block";
+      $searchResultsItems.innerHTML = "";
+      if (term === "") {
+        return;
       }
 
-      $searchResultsItems.appendChild(
-          formatSearchResultItem(results[i], term.split(" ")));
-    }
-  }, 150));
+      const results = index
+        .search(term, options)
+        .filter((r) => r.doc.body !== "");
+      if (results.length === 0) {
+        $searchResultsHeader.innerText = `No search results for '${term}'.`;
+        return;
+      }
+
+      currentTerm = term;
+      $searchResultsHeader.innerText = `${results.length} search results for '${term}':`;
+      for (let i = 0; i < Math.min(results.length, MAX_ITEMS); i++) {
+        if (!results[i].doc.body) {
+          continue;
+        }
+
+        $searchResultsItems.appendChild(
+          formatSearchResultItem(results[i], term.split(" "))
+        );
+      }
+    }, 150)
+  );
 }
 
-if (document.readyState === "complete" ||
-    (document.readyState !== "loading" && !document.documentElement.doScroll)
+if (
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
 ) {
   initToggleMenu();
 } else {
