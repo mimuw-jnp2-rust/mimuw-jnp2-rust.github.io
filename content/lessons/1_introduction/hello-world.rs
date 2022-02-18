ca88086f41a5a09a74363b7d0c9c32ea1f70cd2b
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2022-02-18 09:54:51 +0100

    Add examples from the book to hello-world.rs

--- content/lessons/1_introduction/hello-world.rs
+++ content/lessons/1_introduction/hello-world.rs
@@ -54,4 +54,40 @@ fn main() {
     }
 
     dbg!(x, y);
+
+    labels();
+
+    print!("{}", break_with_value() + 10);
+}
+
+fn labels() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
+}
+
+fn break_with_value() -> i32 {
+    let mut counter = 0;
+    loop {
+        counter += 1;
+        if counter == 10 {
+            break 32;
+        }
+    }
 }
